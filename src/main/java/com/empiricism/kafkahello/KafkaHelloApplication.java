3ac4ee4ed725c7d5dd97a5d82d0510add85952be
michael samuel <micky.samuel@gmail.com>
2024-06-05 11:03:29 +0530

    kafka initial config

---
+++ src/main/java/com/empiricism/kafkahello/KafkaHelloApplication.java
@@ -0,0 +1,13 @@
+package com.empiricism.kafkahello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaHelloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empiricism/kafkahello/KafkaHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empiricism.kafkahello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
